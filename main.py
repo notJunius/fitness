c00ba0e33ebb3ef802297b58cf04465af5d89593
MJ Steel <morgan>
2025-03-23 09:32:19 -0600

    Decided to start this app in the command line

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-from raylib import *
-from pyray import *
-from os.path import join
-
-
 def main():
-    init_window(400, 200, 'fitness app')
-    set_target_fps(60)
-    show_message_box = False
-    
 
-    while not window_should_close():
+    running = True
 
-        begin_drawing()
-        clear_background(GRAY)
-
-        if gui_button(Rectangle(24, 24, 120, 30), "#191#show message"):
-            show_message_box = True
-        
-        if show_message_box:
-            result = GuiMessageBox(Rectangle(85, 70, 250, 100), bytes('#191#message box', 'utf-8'), bytes('This is a message!', 'utf-8'), (bytes('nice;cool;whatever', 'utf-8')))
-            if result >= 0:
-                show_message_box = False
-
-
-
-
-
-        end_drawing()
-
-    close_window()
-
+    while running:
+        pass
 
 
 
@@ -40,3 +14,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
